66353f544771616641d6ab4032f9d62a42f9b542
irsanwoy <irsanwoy54@gmail.com>
2024-10-10 23:56:35 +0700

    setup untuk collor pallete yang akan digunakan

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      primary: '#buat warna utama',
+        secondary: '#warna kedua',
+        accent: '#warna ketiga',
+    },
   },
   plugins: [],
 }
